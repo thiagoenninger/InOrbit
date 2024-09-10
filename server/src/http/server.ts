1f6dc95ab700627c5ba4227f7891a2c79ce2ff01
Thiago Enninger <thiagoenninger@gmail.com>
2024-09-10 18:28:11 -0300

    install fastify-zod dependency and 1st POST created

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,28 @@
 import fastify from 'fastify';
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+import z from 'zod';
+import { createGoal } from '../functions/create-goal';
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post('/goals', {
+  schema: {
+    body: z.object({
+      title: z.string(),
+      desiredWeeklyFrequency: z.number().int().min(1).max(7),
+    })
+  }
+}, async (request) => {
+  const {title, desiredWeeklyFrequency} = request.body
+
+  await createGoal({
+    title,
+    desiredWeeklyFrequency,
+  })
+})
 
 app.listen({
   port: 3333,
