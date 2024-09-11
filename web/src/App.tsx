7912920a761e6144c7cd7509d8d0d1bb8974b213
Thiago Enninger <thiagoenninger@gmail.com>
2024-09-11 17:36:59 -0300

    creating the different app pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,15 @@
-import { Plus } from 'lucide-react'
-
-import logo from './assets/logo-in-orbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+// import { EmptyGoals } from './components/empty-goals'
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="lets.Start" />
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
 
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
-
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
-        <Plus className="size-4" />
-        Cadastrar meta
-      </button>
-    </div>
+      <CreateGoal />
+    </Dialog>
   )
 }
